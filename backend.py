7f73d13e16370b08bd2bf168b3879fde0dd5c32b
vasup86 <vasu08062002@gmail.com>
2022-06-25 21:36:43 -0400

    fixed bugs

--- backend.py
+++ backend.py
@@ -44,7 +44,7 @@ class Backend:
 
     
     def extract(self, page, combUrl):
-        headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
+        headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
         #{}, .format is for the the page# 
         combUrl = (combUrl+ "&start={}").format(page)
         print(combUrl)
